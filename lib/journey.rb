d76a407a641d69a0fd5f8e2798eb02ca16d1153d
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-08 11:18:16 +0000

    some changes

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,24 +2,22 @@
 class Journey
 
   attr_reader :min_fare, :entry_station, :end_station, :fare, :penalty_fare
-  MIN_FARE = 1
+  # MIN_FARE = 1
   PENALTY_FARE = 6
 
-  def initialize(min_fare = MIN_FARE, penalty_fare = PENALTY_FARE)
-    @min_fare = min_fare
+  def initialize(penalty_fare = PENALTY_FARE)
     @penalty_fare = penalty_fare
     @entry_station = nil
     @end_station = nil
   end
 
-  def touch_in(balance, station = nil)
-    insufficient_funds?(balance)
-    @entry_station = station
+  def start(station)
+    @station = station
   end
 
-  def touch_out(station)
-    @end_station = station
-  end
+  def finish(station)
+    @station = station
+ end
 
   def in_journey?
     @entry_station && !@end_station
@@ -39,8 +37,8 @@ class Journey
 
   private
 
-  def insufficient_funds?(balance)
-    fail 'You dont have suffient balance to touch in' if balance < @min_fare
-  end
+  # def insufficient_funds?(balance)
+  #   fail 'You dont have suffient balance to touch in' if balance < @min_fare
+  # end
 
 end
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,14 +3,24 @@ require_relative 'journey'
 class OysterCard
 
   attr_reader :balance #:journey_history
-
+  MIN_FARE = 1
   DEFAULT_BALANCE = 0
   MAX_LIMIT = 90
 
-  def initialize(default_balance = DEFAULT_BALANCE, max_limit = MAX_LIMIT)
+  def initialize(min_fare = MIN_FARE, default_balance = DEFAULT_BALANCE, max_limit = MAX_LIMIT)
+    @min_fare = min_fare
     @balance = default_balance
     @max_limit = max_limit
     # @journey_history = []
+  end
+
+  def touch_in(balance, station = nil)
+    insufficient_funds?(balance)
+    @entry_station = station
+  end
+
+  def touch_out(station)
+    @end_station = station
   end
 
   def top_up(value)
@@ -29,6 +39,10 @@ class OysterCard
   # end
 
   private
+
+  def insufficient_funds?(balance)
+    fail 'You dont have suffient balance to touch in' if balance < @min_fare
+  end
 
   def exceed_max_limit?(value)
     (@balance + value) > @max_limit
